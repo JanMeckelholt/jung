fd734c6c92e5a1c06cd11fd3512fde9f49db1d57
jmeckel <jan.meckelholt@mercedes-benz.com>
2025-03-14 20:17:46 +0100

    set topic for mqtt message

--- jung/main.go
+++ jung/main.go
@@ -42,8 +42,7 @@ func Handler() http.Handler {
 					return
 				}
 				log.Infof("body: %s", body)
-				mqtt.Publish(mqtt.Client, "jung", string(body))
-				mqtt.Publish(mqtt.Client, "jung", req.URL.Path)
+				mqtt.Publish(mqtt.Client, "req.URL.Path", string(body))
 
 				rw.WriteHeader(http.StatusOK)
 				res, err := rw.Write(body)
